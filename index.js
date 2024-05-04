62f4b16896557097683830f52ab5760f58554b20
DenpaiID <rayhanalfarezy0@gmail.com>
2024-05-04 23:04:55 +0700

    V1

---
+++ index.js
@@ -0,0 +1,252 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const axios = require('axios');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.enable("trust proxy");
+app.set("json spaces", 2);
+
+// Middleware untuk CORS
+app.use(cors());
+
+// Fungsi untuk ragBot
+async function ragBot(message) {
+  try {
+    const response = await axios.post('https://ragbot-starter.vercel.app/api/chat', {
+      messages: [{ role: 'user', content: message }],
+      useRag: true,
+      llm: 'gpt-3.5-turbo',
+      similarityMetric: 'cosine'
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Fungsi untuk degreeGuru
+async function degreeGuru(message, prompt) {
+  try {
+    const response = await axios.post('https://degreeguru.vercel.app/api/guru', {
+      messages: [
+        { role: 'user', content: message }
+      ]
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Fungsi untuk pinecone
+async function pinecone(message) {
+  try {
+    const response = await axios.post('https://pinecone-vercel-example.vercel.app/api/chat', {
+      messages: [{ role: 'user', content: message }]
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Fungsi untuk smartContract
+async function smartContract(message) {
+  try {
+    const response = await axios.post("https://smart-contract-gpt.vercel.app/api/chat", {
+      messages: [{ content: message, role: "user" }]
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function blackboxAIChat(message) {
+  try {
+    const response = await axios.post('https://www.blackbox.ai/api/chat', {
+      messages: [{ id: null, content: message, role: 'user' }],
+      id: null,
+      previewToken: null,
+      userId: null,
+      codeModelMode: true,
+      agentMode: {},
+      trendingAgentMode: {},
+      isMicMode: false,
+      isChromeExt: false,
+      githubToken: null
+    });
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+// jnt 
+async function cekjnt(resi) {
+  const response = await axios.get(`https://api.binderbyte.com/v1/track?api_key=4bda755e0c2db98f4ba36a8d14d94543a13313af7caa3bf89db6b3e948510db4&courier=jnt&awb=${resi}`);
+   return response.data;
+     } catch (error) {
+    throw error;
+  }
+}
+
+// Fungsi cepat
+/*async function sicepat(resi) {
+  const response = await axios.get(`https://api.binderbyte.com/v1/track?api_key=4bda755e0c2db98f4ba36a8d14d94543a13313af7caa3bf89db6b3e948510db4&courier=sicepat&awb=${resi}`);
+   return response.data;
+}
+
+// Fungsi POST
+async function post(resi) {
+  const response = await axios.get(`https://api.binderbyte.com/v1/track?api_key=4bda755e0c2db98f4ba36a8d14d94543a13313af7caa3bf89db6b3e948510db4&courier=post&awb=${resi}`);
+   return response.data;
+}
+
+// Funsi tiki
+async function tiki(resi) {
+  const response = await axios.get(`https://api.binderbyte.com/v1/track?api_key=4bda755e0c2db98f4ba36a8d14d94543a13313af7caa3bf89db6b3e948510db4&courier=tiki&awb=${resi}`);
+   return response.data;
+}
+
+// Fungsi Lion 
+async function lion(resi) {
+  const response = await axios.get(`https://api.binderbyte.com/v1/track?api_key=4bda755e0c2db98f4ba36a8d14d94543a13313af7caa3bf89db6b3e948510db4&courier=lion&awb=${resi}`);
+   return response.data;
+}
+*/
+// Endpoint untuk servis dokumen HTML
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/pages/home.html'));
+});
+
+app.get('/docs', (req, res) => {
+  res.sendFile(path.join(__dirname, '/pages/docs.html'));
+});
+
+// Endpoint untuk ragBot
+app.get('/api/ragbot', async (req, res) => {
+  try {
+    const message = req.query.message;
+    if (!message) {
+      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
+    }
+    const response = await ragBot(message);
+    res.status(200).json({
+      status: 200,
+      creator: "siputzx",
+      data: { response }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Endpoint untuk degreeGuru
+app.get('/api/degreeguru', async (req, res) => {
+  try {
+    const { message }= req.query;
+    if (!message) {
+      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
+    }
+    const response = await degreeGuru(message);
+    res.status(200).json({
+      status: 200,
+      creator: "siputzx",
+      data: { response }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Endpoint untuk pinecone
+app.get('/api/pinecone', async (req, res) => {
+  try {
+    const message = req.query.message;
+    if (!message) {
+      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
+    }
+    const response = await pinecone(message);
+    res.status(200).json({
+      status: 200,
+      creator: "siputzx",
+      data: { response }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Endpoint untuk smartContract
+app.get('/api/smartcontract', async (req, res) => {
+  try {
+    const message = req.query.message;
+    if (!message) {
+      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
+    }
+    const response = await smartContract(message);
+    res.status(200).json({
+      status: 200,
+      creator: "siputzx",
+      data: { response }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Endpoint untuk blackboxAIChat
+app.get('/api/blackboxAIChat', async (req, res) => {
+  try {
+    const message = req.query.message;
+    if (!message) {
+      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
+    }
+    const response = await blackboxAIChat(message);
+    res.status(200).json({
+      status: 200,
+      creator: "siputzx",
+      data: { response }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get('/api/cekjnt', async (req, res) => {
+  try {
+    const message = req.query.message;
+    if (!message) {
+      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
+    }
+    const response = await blackboxAIChat(message);
+    res.status(200).json({
+      status: 200,
+      creator: "siputzx",
+      data: { response }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Handle 404 error
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!");
+});
+
+// Handle error
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// Jalankan server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app
